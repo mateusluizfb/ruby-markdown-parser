6173ef296ac4233aa20ec2c9a6bad977af0f6396
Mateus Luiz <mateus@novatics.com.br>
2019-08-23 14:25:29 -0300

    TCR STEP

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,5 +1,15 @@
 class Parser
-  def self.parse_markdown(markdown_text)
+  def self.parse(markdown_text)
+    if markdown_text.include? "_"
+      italic(markdown_text.gsub! '_', '')
+    else
+      paragraph(markdown_text)
+    end
+  end
+  def self.paragraph(markdown_text)
     "<p>#{markdown_text}</p>"
+  end
+  def self.italic(markdown_text)
+    "<em>#{markdown_text}</em>"
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Parser do
   it 'parse a paragraph' do
     markdown_text = 'This is a simple text'
     paragraph = '<p>This is a simple text</p>'
-    result = Parser.parse_markdown(markdown_text)
+    result = Parser.paragraph(markdown_text)
     expect(result).to eq(paragraph)
+  end
+
+  it 'pass italic' do
+    expect(true)
   end
 end
