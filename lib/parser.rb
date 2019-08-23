bdf2cf700607064c109d3f7479e7f5fd1989115e
Mateus Luiz <mateus@novatics.com.br>
2019-08-23 13:16:22 -0300

    TCR STEP

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,19 +1,6 @@
 module Parser
   def self.parse_markdown(markdown_text)
-    if markdown_text[0] == '*'
-      list = "<ul>"
-      list.concat("<li>#{remove_list_symbol(markdown_text)}</li>")
-      list.concat("</ul>")
-      return list
-    end
-
     "<p>#{replace_tags(markdown_text)}</p>"
-  end
-
-  def self.remove_list_symbol(list_item)
-    list_item
-      .gsub(/([*][ ]|[*])/, '')
-      .sub(/(\\n)/, '')
   end
 
   def self.replace_tags(markdown_text)
