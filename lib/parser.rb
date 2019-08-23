c127f728a75da6e7748c5b76fc81cfff53155b29
Mateus Luiz <mateus@novatics.com.br>
2019-08-23 13:17:09 -0300

    TCR STEP

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,11 +1,5 @@
 module Parser
   def self.parse_markdown(markdown_text)
-    "<p>#{replace_tags(markdown_text)}</p>"
-  end
-
-  def self.replace_tags(markdown_text)
-    markdown_text
-      .then { |text| text.gsub(/__(.*?)\__/, '<strong>\1</strong>') }
-      .then { |text| text.gsub(/_(.*?)\_/, '<em>\1</em>') }
+    "<p>#{markdown_text}</p>"
   end
 end
